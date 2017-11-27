37c28f39fa98c54ddeec4f4e8a4307a03fc11064
neo <neo.sunny2k13@gmail.com>
2017-11-27 13:44:11 +0530

    Updated Menu component to use Dish Service

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
 
+import { DishService } from '../services/dish.service';
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -13,14 +15,15 @@ import { DISHES } from '../shared/dishes';
 
 export class MenuComponent implements OnInit {
 
-  dishes= DISHES;
+  dishes: Dish[];
   selectedDish: Dish;
 
-  constructor() { }
+  constructor(private dishService: DishService) { }
   onSelect(dish: Dish){
     this.selectedDish=dish;
   }
   ngOnInit() {
+    this.dishes=this.dishService.getDishes();
   }
 
 }

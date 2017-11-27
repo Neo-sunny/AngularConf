464b0196ed85c1f45d225fd114ee0189a3bcb6f8
neo <neo.sunny2k13@gmail.com>
2017-11-27 21:15:04 +0530

    Creating the promotion service and its components

---
+++ src/app/services/promotion.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Promotion } from '../shared/promotion';
+import { PROMOTIONS } from '../shared/promotions';
+
+@Injectable()
+export class PromotionService {
+
+  constructor() { }
+
+}

12fc11f5c2dcbd1c7199aa41fa606466bd49d158
neo <neo.sunny2k13@gmail.com>
2017-11-27 19:39:55 +0530

    Updating the contact to use router link

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core/src/metadata/directives';
 
 import { HomeComponent } from '../home/home.component';
 import { MenuComponent } from '../menu/menu.component';
+import { ContactComponent } from '../contact/contact.component';
 
 export const routes: Routes=[
     {path:'home', component:HomeComponent},
     {path: 'menu' ,component:MenuComponent},
+    {path: 'contactus', component:ContactComponent},
     { path: '' , redirectTo:'home', pathMatch:'full'},
 ];

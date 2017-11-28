b9302031f8cb117b477338dae22af2a8f29a4253
neo <neo.sunny2k13@gmail.com>
2017-11-28 09:01:49 +0530

    updating the project to use routerlink for internal navigation and removing the master detail structure

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -4,10 +4,12 @@ import { Component } from '@angular/core/src/metadata/directives';
 import { HomeComponent } from '../home/home.component';
 import { MenuComponent } from '../menu/menu.component';
 import { ContactComponent } from '../contact/contact.component';
+import { DishdetailComponent } from '../dishdetail/dishdetail.component';
 
 export const routes: Routes=[
     {path:'home', component:HomeComponent},
     {path: 'menu' ,component:MenuComponent},
     {path: 'contactus', component:ContactComponent},
+    {path:'dishdetail/:id', component:DishdetailComponent},
     { path: '' , redirectTo:'home', pathMatch:'full'},
 ];
